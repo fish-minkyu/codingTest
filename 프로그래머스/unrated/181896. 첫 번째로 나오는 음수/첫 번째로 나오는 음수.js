918e2c4012298e3937ef9c21be4a4361f9864774
fish-minkyu <128130163+fish-minkyu@users.noreply.github.com>
2023-06-18 12:36:24 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let result = 0
+    for (let i = 0; i < num_list.length; i++) {
+        if (num_list[i] > 0) {
+            result = -1}
+        else {
+           return result = num_list.indexOf(num_list[i])
+        }
+    }
+    return result
+}
