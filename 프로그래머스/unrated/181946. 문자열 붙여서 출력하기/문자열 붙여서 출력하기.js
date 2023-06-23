ebe5930b475f05f154741d48118a528cadc6d4ca
fish-minkyu <128130163+fish-minkyu@users.noreply.github.com>
2023-06-23 23:56:53 +0900

    [unrated] Title: 문자열 붙여서 출력하기, Time: 42.39 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' '); // ['apple', 'pen']
+    console.log(input.join(''))
+    
+    
+    
+}).on('close', function () {
+    str1 = input[0];
+    str2 = input[1];
+});
