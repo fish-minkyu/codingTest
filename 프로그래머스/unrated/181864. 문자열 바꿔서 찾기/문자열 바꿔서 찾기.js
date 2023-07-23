6879b239dee9f42f9c95536436caf7a74d6b944c
fish-minkyu <128130163+fish-minkyu@users.noreply.github.com>
2023-07-23 14:48:13 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,12 +1,4 @@
 function solution(myString, pat) {
- let str = ''
-
-  for (let i = 0; i < myString.length; i++) {
-    if (myString[i] == "A") {
-      str += "B"
-    } else {
-      str += "A"
-    }
-  }
-  return str.indexOf(pat) +1 ? 1 : 0
+    var answer = 0;
+    return answer;
 }
