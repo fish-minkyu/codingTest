64ea2e5998048d6a136846461d6528a87a80b15f
fish-minkyu <128130163+fish-minkyu@users.noreply.github.com>
2023-08-18 16:36:10 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,17 @@
+function solution(num) {
+  if (num === 1) return 0
+
+  let cnt = 0
+  while(num >1) {
+    if(num % 2 == 0) {
+      num = num / 2
+    } else {
+      num = num * 3 + 1
+    }
+    cnt++
+  }
+
+  if (cnt > 500) return -1
+  
+  return cnt
+}
