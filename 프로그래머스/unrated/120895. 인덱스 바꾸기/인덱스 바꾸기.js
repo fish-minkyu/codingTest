2d4d8dfb2de93688c4befdb4f9d21076378c61a5
어민규 <128130163+fish-minkyu@users.noreply.github.com>
2023-11-03 08:38:54 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,7 @@
+function solution(my_string, num1, num2) {
+  return my_string.slice(0, num1) 
+       + my_string.slice(num2, num2 +1)  
+       + my_string.slice(num1 +1, num2)
+       + my_string.slice(num1, num1 +1) 
+       + my_string.slice(num2 + 1, my_string.length)
+}
