633180948a205fed8b6dc5ae653ce10fe3e66230
어민규 <128130163+fish-minkyu@users.noreply.github.com>
2023-12-05 13:15:45 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.49 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Solution {
+  public static int solution(int a, int b, int c) {
+    // 정렬을 하기 위해 배열을 나열한다.
+    int[] numbers = {a, b, c};
+    // 정렬을 한다.
+    Arrays.sort(numbers);
+
+    // 1. 0번 원소와 2번 원소가 동일하면 3개 일치
+    if (numbers[0] == numbers[2]) {
+      return (a + b + c) *
+        (a * a + b * b + c * c) *
+        (a * a * a + b * b * b + c * c * c);
+    }
+    // 2. 1번이 아니라면, 0번 원소와 1번 원소가 같거나 1번 원소와 2번원소가 같거나
+    // (0번과 2번은 같을 수 없다.)
+    else if (numbers[0] == numbers[1] || numbers[1] == numbers[2]) {
+      return (a + b + c) *
+        (a * a + b * b + c * c);
+    }
+    // 아니면 다 다르다.
+    else {
+      return a + b + c;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(4, 4, 4));
+  }
+}
