1c30b78de32a130bfe479902f6a59814a2083be9
어민규 <128130163+fish-minkyu@users.noreply.github.com>
2023-12-08 09:10:51 +0900

    [level unrated] Title: qr code, Time: 12.15 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.java
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -1,15 +1,12 @@
 class Solution {
   public String solution(int q, int r, String code) {
-    // code를 배열로 만든다.
-    String[] str = code.split("");
-    
-    String result = "";
-   // 반복문, 각 인덱스를 q로 나눠 r인 것들을 배열에 담는다.
+   String answer = "";
+   // for 반복을 한다.
     for (int i = 0; i < code.length(); i++) {
       if (i % q == r) {
-        result += str[i];
+      answer = answer + code.charAt(i);
       }
     }
-   return result;
+    return answer;
   }
 }
