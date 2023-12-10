1edc351d9196dec64b78e31126cbdf160394b385
어민규 <128130163+fish-minkyu@users.noreply.github.com>
2023-12-10 21:32:07 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=java) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.05 ms
+메모리: 78.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 11:18:25
+2023년 12월 0일 21:32:5
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,26 @@
+public class Solution {
+    /*
+  1. 찾을 단어와 카드 뭉치의 제일 앞에 있는 것과 비교해서 찾은 단어라면 소멸
+  2. 각각의 카드뭉치의 첫째 장을 꺼내 goal의 첫째 장과 일치하는지 확인
+  3. 일치하면 해당 카드뭉치는 다음 장으로 이동
+  */
+  public String solution(String[] cards1, String[] cards2, String[] goal) {
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < goal.length; i++) {
+      if (a < cards1.length && goal[i].equals(cards1[a])) {
+        a++;
+        continue;
+      }
+
+      if (b < cards2.length && goal[i].equals(cards2[b])) {
+        b++;
+        continue;
+      }
+
+      return "No";
+    }
+
+    return "Yes";
+  }
+}
