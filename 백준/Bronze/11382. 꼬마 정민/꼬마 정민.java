310b8c0ceaca713e7934c7c859d216f3ccef4f14
어민규 <128130163+fish-minkyu@users.noreply.github.com>
2023-12-18 17:18:44 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader =
+      new BufferedReader(new InputStreamReader(System.in));
+
+    String[] input = reader.readLine().split(" ");
+    long A = Long.parseLong(input[0]);
+    long B = Long.parseLong(input[1]);
+    long C = Long.parseLong(input[2]);
+
+    System.out.println(A + B + C);
+  }
+}
