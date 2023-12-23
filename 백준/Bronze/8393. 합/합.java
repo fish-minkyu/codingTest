6b941d459633f398c655193cc138a77f09318c8d
어민규 <128130163+fish-minkyu@users.noreply.github.com>
2023-12-23 12:49:30 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader =
+      new BufferedReader(new InputStreamReader(System.in));
+
+    int num = Integer.parseInt(reader.readLine());
+
+    int sum = 0;
+    for (int i = 1; i <= num; i++) {
+      sum += i;
+    }
+
+    System.out.println(sum);
+  }
+}
