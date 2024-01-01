e1440a9af1752f74679bb3e52c4987ff49a53c95
어민규 <128130163+fish-minkyu@users.noreply.github.com>
2024-01-01 13:10:57 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader =
+      new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
+    
+    int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+    int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+    System.out.println(a > b ? a : b);
+  }
+}
