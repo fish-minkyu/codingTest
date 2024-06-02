d6697983a47b22a5f464dc3d5dbd6cb13f6c466f
어민규 <e951219@naver.com>
2024-06-02 12:40:10 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int solution(int[] num_list) {
+    int idx = -1;
+    
+    for (int i = 0; i < num_list.length; i++) {
+      if (num_list[i] < 0) return idx = i;
+    }
+    
+    return idx;
+  }
+}
