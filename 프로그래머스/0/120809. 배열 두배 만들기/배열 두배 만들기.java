59422175e0a4623cde6938d4ad09d1d7e0ef5d2f
어민규 <e951219@naver.com>
2024-06-29 13:13:59 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,9 @@
+class Solution {
+  public int[] solution(int[] numbers) {
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = numbers[i] * 2;
+    }
+    
+    return numbers;
+  }
+}
