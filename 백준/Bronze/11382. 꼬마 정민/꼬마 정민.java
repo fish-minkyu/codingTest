baf8898d4352bd0b12e0b4117600b380f09bd7d6
어민규 <e951219@naver.com>
2025-09-13 21:36:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -3,15 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader reader =
-      new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader =
+            new BufferedReader(new InputStreamReader(System.in));
 
-    String[] input = reader.readLine().split(" ");
-    long A = Long.parseLong(input[0]);
-    long B = Long.parseLong(input[1]);
-    long C = Long.parseLong(input[2]);
+        String[] input = reader.readLine().split(" ");
+        long A = Long.parseLong(input[0]);
+        long B = Long.parseLong(input[1]);
+        long C = Long.parseLong(input[2]);
 
-    System.out.println(A + B + C);
-  }
+        System.out.println(A + B + C);
+    }
 }
