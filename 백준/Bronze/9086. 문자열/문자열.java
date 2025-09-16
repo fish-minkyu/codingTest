243e617a9bdcbcc54dca73ba361a3d9ebc228338
어민규 <e951219@naver.com>
2025-09-16 14:37:12 +0900

    [Bronze V] Title: 문자열, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -3,17 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader reader
-      = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    int count = Integer.parseInt(reader.readLine());
-    for (int i = 0; i < count; i++) {
-      String input = reader.readLine();
-      String first = String.valueOf(input.charAt(0));
-      String last = String.valueOf(input.charAt(input.length() -1));
-      System.out.println(String.valueOf(first + last));
+        int count = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < count; i++) {
+            String str = reader.readLine();
+
+            StringBuilder answer = new StringBuilder();
+            answer.append(str.charAt(0)).append(str.charAt(str.length() - 1));
+            System.out.println(answer);
+        }
     }
-    reader.close();
-  }
 }
